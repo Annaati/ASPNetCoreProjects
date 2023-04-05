f2a5c689eadc32d8de5344e328ca553af4e74b62
Annaati <Abdi17fatah@gmail.com>
2023-04-05 23:23:57 +0300

    DIExample: initial Home Controller

---
+++ DIExample/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IServices;
+
+namespace DIExample.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        private readonly ICountriesService _countriesService;
+
+        public HomeController(ICountriesService countriesService)
+        {
+            _countriesService = countriesService;
+        }
+
+        public IActionResult Index()
+        {
+            List<string> countries = _countriesService.GetCountries();
+            return View();
+        }
+    }
+}
