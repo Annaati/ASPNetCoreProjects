db8a75f5f599513415331a6facfe3335d654ca77
Annaati <Abdi17fatah@gmail.com>
2023-04-06 12:07:20 +0300

    DI Example: implemented GetCountries() of ICountriesServices

--- DIExample/Services/CountriesService.cs
+++ DIExample/Services/CountriesService.cs
@@ -2,8 +2,27 @@
 
 namespace Services
 {
-    public class Countries //: IServices
+    public class CountriesService : ICountriesService
     {
 
+        private List<string> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<string>() {
+                "1. Somalia",
+                "2. Palestine",
+                "3. Turkey",
+                "4. Pakistan",
+                "5. Indonesia",
+                "6. Malasia",
+                "7. Maldives"
+            };
+        }
+
+        public List<string> GetCountries()
+        {
+            return _countries;
+        }
     }
 }
