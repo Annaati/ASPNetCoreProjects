dcee6310e449e5fda3d7e1c4d3e9f34cb1e4978d
Annaati <Abdi17fatah@gmail.com>
2023-04-06 15:20:18 +0300

    DIAssigmnet-WeatherApp: Models Class Library Projects -CityWeather Model

---
+++ DIAssignMent-WeatherApp/Models/CityWeather.cs
@@ -0,0 +1,18 @@
+﻿namespace Models
+{
+    public class CityWeather
+    {
+
+        public int SQN { get; set; }
+
+        public string CityCode { get; set; }
+
+        public string CountryName { get; set; }
+        public string CityName { get; set; }
+
+        public DateTime DateAndTime { get; set; }
+
+        public int TemperatureFahrenheit { get; set; }
+
+    }
+}
