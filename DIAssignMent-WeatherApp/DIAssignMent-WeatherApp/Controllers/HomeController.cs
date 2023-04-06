03c87307b25dbea3b51a182ba8fd26e1782c4cc6
Annaati <Abdi17fatah@gmail.com>
2023-04-06 15:18:13 +0300

    DIAssigmnet-WeatherApp: Implemented Home Controller - List CitiesWeather & WeathetrByCityCode

---
+++ DIAssignMent-WeatherApp/DIAssignMent-WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using ServicesInterface;
+
+namespace DIAssignMent_WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWeatherService _weatherService;
+        public HomeController(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+
+            return View(_weatherService.GetWeatherDetails());
+        }
+
+        [Route("Weather/{Citycode}")]
+        public IActionResult WeatherByCity(string? CityCode)
+        {
+            if(string.IsNullOrWhiteSpace(CityCode))
+            {
+                return View();
+            }
+
+            CityWeather city = _weatherService.GetWeatherByCity(CityCode);
+            return View(city);
+        }
+
+
+    }
+}
