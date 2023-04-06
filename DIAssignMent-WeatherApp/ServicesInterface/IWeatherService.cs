12e35a6b776abea5cceb8379d621447724ee77f1
Annaati <Abdi17fatah@gmail.com>
2023-04-06 15:21:53 +0300

    DIAssigmnet-WeatherApp: Services Interface Class Library Project - IWeather - GetWeatherDetails() & GetWeatherByCity()

---
+++ DIAssignMent-WeatherApp/ServicesInterface/IWeatherService.cs
@@ -0,0 +1,11 @@
+﻿using Models;
+
+namespace ServicesInterface
+{
+    public interface IWeatherService
+    {
+
+        List<CityWeather> GetWeatherDetails();
+        CityWeather GetWeatherByCity(string CityCode);
+    }
+}
