41e46d196fe300f4959d1f6a0d8400c1655c277c
Annaati <Abdi17fatah@gmail.com>
2023-04-25 03:44:04 +0300

    ServiceContracts : Added CountryAddRes DTO Class with Properties

---
+++ CrudExampleSol/ServiceContracts/DTO/CountryRes.cs
@@ -0,0 +1,43 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class that is sued as return type for most CountriesService Methods
+    /// </summary>
+    public class CountryRes
+    {
+        public Guid CountryId { get; set; }
+        public string? CountryName { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType() != typeof(CountryRes)) return false;
+
+            CountryRes countryToCompare = (CountryRes)obj;
+
+            return CountryId == countryToCompare.CountryId && CountryName == countryToCompare.CountryName;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+
+
+    public static class CountryExtensions
+    {
+        public static CountryRes ToCountryResponse(this Country country)
+        {
+            return new CountryRes() { CountryId = country.CountryId, CountryName = country.CountryName };
+        }
+    }
+
+}
