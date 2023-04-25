29eaab5c89e575fd9c823ea719d0b6635ff76fc0
Annaati <Abdi17fatah@gmail.com>
2023-04-25 03:38:43 +0300

    Entities : Added Person with properties

---
+++ CrudExampleSol/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Entities
+{
+    /// <summary>
+    /// Person domain model class
+    /// </summary>
+    public class Person
+    {
+        public Guid PersonId { get; set; }
+        public Guid? CountryId { get; set; }
+        public string PersonName { get; set; }
+        public string? Sex { get; set; }
+        public DateTime? DOB { get; set; }
+        public string Email { get; set; }
+        public string? Address { get; set; }
+        public bool RecieveNewsLetters { get; set; }
+    }
+}
