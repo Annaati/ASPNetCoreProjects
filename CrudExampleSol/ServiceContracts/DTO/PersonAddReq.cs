e3843991be95d6f2f4112077273a871120557775
Annaati <Abdi17fatah@gmail.com>
2023-04-25 03:44:37 +0300

    ServiceContracts : Added PersonAddReq DTO Class with Properties

---
+++ CrudExampleSol/ServiceContracts/DTO/PersonAddReq.cs
@@ -0,0 +1,50 @@
+﻿using Entities;
+using ServiceContracts.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceContracts.DTO
+{
+    public class PersonAddReq
+    {
+
+        [Display(Name = "Country Id")]
+        public Guid? CountryId { get; set; }
+
+        [Required(ErrorMessage = "Person Name is required")]
+        [Display(Name = "Person Name")]
+        public string PersonName { get; set; }
+
+        [Required(ErrorMessage = "Person Sex is required")]
+        public SexEnums? Sex { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime? DOB { get; set; }
+
+        [Required(ErrorMessage = "Person Email is required")]
+        [EmailAddress(ErrorMessage = "Email Mmust be a valid Email Address")]
+        public string Email { get; set; }
+
+        public string? Address { get; set; }
+
+        public bool RecieveNewsLetters { get; set; }
+
+
+        /// <summary>
+        /// Coverts The Current of PersonAddReq into a new object of the Person type
+        /// </summary>
+        /// <returns>Person object</returns>
+        public Person ToPerson()
+        {
+            return new Person()
+            {
+                CountryId = CountryId,
+                PersonName = PersonName,
+                Sex = Sex.ToString(),
+                DOB = DOB,
+                Email = Email,
+                Address = Address,
+                RecieveNewsLetters = RecieveNewsLetters
+            };
+        }
+    }
+}
