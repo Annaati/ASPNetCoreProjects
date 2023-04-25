d28f6f223a5504578ab32248f29eb7b03f7d03e0
Annaati <Abdi17fatah@gmail.com>
2023-04-25 03:52:26 +0300

    Services : Added Model Validation Helper Clas

---
+++ CrudExampleSol/Services/Helpers/ModelValidationHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Services.Helpers
+{
+    public class ModelValidationHelper
+    {
+
+        internal static void ModelValidation(Object obj)
+        {
+            ValidationContext validationContext = new ValidationContext(obj);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            bool IsValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+
+            if (!IsValid)
+                throw new ArgumentException(validationResults.FirstOrDefault()?.ErrorMessage);
+
+        }
+    }
+}
