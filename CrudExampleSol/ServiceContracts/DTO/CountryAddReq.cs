c170b12e0b46591fcdf81528b03dd6c4061c717b
Annaati <Abdi17fatah@gmail.com>
2023-04-25 03:43:32 +0300

    ServiceContracts : Added CountryAddReq DTO Class with Properties

---
+++ CrudExampleSol/ServiceContracts/DTO/CountryAddReq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace ServiceContracts.DTO
+{
+    public class CountryAddReq
+    {
+        /// <summary>
+        /// DTO Class for add a new Country
+        /// </summary>
+        public string? CountryName { get; set; }
+
+
+        public Country ToCountry()
+        {
+            return new Country() { CountryName = CountryName };
+        }
+    }
+}
